187bbfd38226621ed349c2220095c5d4008f70fa
Roman <sparrowroman@yandex.ru>
2021-08-19 20:56:28 +0300

    Initial commit

---
+++ src/Kalkulyator/Main.java
@@ -0,0 +1,30 @@
+package Kalkulyator;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[]agrs) {
+        System.out.println("Введите операцию:");
+        System.out.println("1. Сложение ");
+        System.out.println("2. Вычитание ");
+        System.out.println("3. Умножение ");
+        System.out.println("4. Деление ");
+        Scanner scanner = new Scanner(System.in);
+        int operation = scanner.nextInt();
+        System.out.println("Введите первое значение ");
+        int a = scanner.nextInt();
+        System.out.println("Введите второе значение ");
+        int b = scanner.nextInt();
+        int result;
+        if (operation == 1) {
+            result = a+b;
+        } else if (operation == 2) {
+            result = a-b;
+        } else if (operation == 3) {
+            result = a*b;
+        } else  {
+            result = a/b;
+        }
+        System.out.println("Результат = " + result);
+    }
+    }
